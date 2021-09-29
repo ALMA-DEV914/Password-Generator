0672b831980779d599d97a2bc02a756885ed0e97
ALMA_DEV914 <aungonalna58@gamil.com>
2021-09-29 12:57:37 -0700

    Fixed the alerts and prompts function

--- script.js
+++ script.js
@@ -8,52 +8,52 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // alert the user to enter their password
+    // alert the user to enter their password
   window.alert("Please generate your new password.");
-  // alert user the criteria of the password
   window.alert("Password should contain upperCase, lowerCase, number and at least one special character.")
- // ask the user if they'd like to add one of the criteria
- var promptConfirm = window.prompt("Would you like to add  'lowerCase', 'upperCase', 'number', 'specialCharacter' in your password? Answer YES or NO");
 
- // ask user to confirm if they leave the prompt empty
- if (promptConfirm === "") {
-   // alert user to confirm Yes or No
-  window.alert("Cannot be empty, please answer YES or NO to confirm");
-  // ask user if they'd like to add the criteria's for password again
   var promptConfirm = window.prompt("Would you like to add  'lowerCase', 'upperCase', 'number', 'specialCharacter' in your password? Answer YES or NO");
-  // if user confirm, ask to choose the criteria
-} else {
-  window.prompt("Choose from the following: 'lowerCase', 'upperCase', 'number', 'specialCharacter'.");
-}
-// ask user to choose if they confirm YES
-if (promptConfirm === "YES") {
-  window.alert("Choose from the following: 'lowerCase', 'upperCase', 'number', 'specialCharacter'.");
-// ask user to choose the criteria they'd like to add
-  var promptConfirm = window.prompt("Choose: 'lowerCase', 'upperCase', 'number', 'specialCharacter'.");
-  // alert them about the password length
- var passwordLength = window.prompt("The password length should be minimum of 8 character and not more than 128 character", randomNumber);
-// if user confirm NO(false)
-} else if (promptConfirm === "NO") {
-  // alert user to force them to choose at least one criteria
-    window.alert("You have to choose at least one criteria. Try again");
-    // prompt the criteria again for them to choose from
-   var promptConfirm = window.prompt("Choose: 'lowerCase', 'upperCase', 'number', 'specialCharacter'.");
-   // 
-   var passwordLength = (8, 128);
-   var passwordLength = window.prompt("The password length should be minimum of 8 character and not more than 128 character");
-   // alert the password
-} else {
-    generatePassword();
-}
+  
+  if (promptConfirm === "") {
+    window.alert("Cannot be empty, please answer YES or NO to confirm");
+    var promptConfirm = window.prompt("Would you like to add  'lowerCase', 'upperCase', 'number', 'specialCharacter' in your password? Answer YES or NO");
+    var promptConfirm = window.prompt("Choose from the following: 'lowerCase', 'upperCase', 'number', 'specialCharacter'.");
+    var promptConfirm = window.prompt("The password length should be minimum of 8 character and not more than 128 character. Enter your password length.");
+  } else if (promptConfirm === "YES") {
 
+    var promptConfirm = window.prompt("Choose from the following: 'lowerCase', 'upperCase', 'number', 'specialCharacter'.");
+ 
+     window.prompt("The password length should be minimum of 8 character and not more than 128 character. Enter the length of your password");
+
+   var password = window.prompt("Please key in your password.");
+   generatePassword(randomNumber);
+
+ } else if (promptConfirm === "NO") {
+      window.alert("You have to choose at least one criteria. Try again");
+
+     var promptConfirm = window.prompt("Choose: 'lowerCase', 'upperCase', 'number', 'specialCharacter'.");
+
+      window.prompt("The password length should be minimum of 8 character and not more than 128 character");
+  } else {
+      window.prompt("The password length should be minimum of 8 character and not more than 128 character");
+  }
+var password = window.prompt("Please key in your password.", randomNumber);
 // asign password to generatePassword() function
  var password = generatePassword();
-     
+  
+ function generatePassword() {
+  var password = "";
+  var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjiklmnopqrstuvwxyz0123456789@#$%&*";
+  var passwordLength = 14;
+for ( var i = 0; i < passwordLength; i++) {
+var randomNumber = Math.floor(Math.random() * charset.length);
+password += charset.substring(randomNumber, randomNumber + 1);
+   
+  }
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
- 
+   }
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
