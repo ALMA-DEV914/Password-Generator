4b792b66bd9b303e1108d6ff5f254369c1fa862f
ALMA_DEV914 <aungonalna58@gamil.com>
2021-09-29 09:43:54 -0700

    create the variables for the password

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 // Assignment code here
-
+var password = "";
+var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjiklmnopqrstuvwxyz0123456789@#$%&*";
+var passwordLength = 14;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  
+ var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
