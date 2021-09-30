855a903370ef64842e65bd1ba6e783469372f526
ALMA_DEV914 <aungonalna58@gamil.com>
2021-09-30 14:47:01 -0700

    add function to get the password length

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 var password = "";
-var passwordLength = 14;
+var passwordLength = "";
 var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjiklmnopqrstuvwxyz0123456789@#$%&*";
 
  
@@ -20,27 +20,34 @@ var promptConfirm = "";
   
     window.prompt("Would you like to add uppercase?");
   
-      window.prompt("Would you like to add number?")
+    window.prompt("Would you like to add number?")
   
     window.prompt("Would you like to add a specialCharacter?");
-  
-   var passwordLength = window.prompt("Choose the length of your password between 8 and 128 characters.");
-  }
+
+
+  var promptPasswordLength =  window.prompt("Choose the length of your password between 8 and 128 characters.");
+    
+} else if (function passwordLength(min, max) {
+      let passwordLength = 8 + "";
+      if (passwordLength === 8 >= 128 ){
+         return passwordLength();
+      }
+});
    else{
     generatePassword();
   }
-  
+
   var promptPassword = window.prompt("Please key in your password");
   
  var password = generatePassword();
   
 function generatePassword() {
     var password = "";
-    var passwordLength = 14;
+    var passwordLength = 24;
     var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjiklmnopqrstuvwxyz0123456789@#$%&*";
-for ( var i = 0; i < passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * charset.length);
-  password += charset.substring(randomNumber, randomNumber + 1);
+for ( var i = 8; i < passwordLength; i++) {
+  var randomPassword = Math.floor(Math.random() * charset.length);
+  password += charset.substring(randomPassword,randomPassword + 1);
  }
 
 var passwordText = document.querySelector("#password");
